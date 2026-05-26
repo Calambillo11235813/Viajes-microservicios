08df512115c33e21c72f05d1a0c1518e1e2c6f21
Calambillo <nicolas58772@gmail.com>
2026-05-25 21:06:12 -0400

    first commit

---
+++ core-transaccional/src/main/java/com/agencia/viajes/transaccional/CoreTransaccionalApplication.java
@@ -0,0 +1,13 @@
+package com.agencia.viajes.transaccional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTransaccionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreTransaccionalApplication.class, args);
+	}
+
+}
---
+++ core-transaccional/src/test/java/com/agencia/viajes/transaccional/CoreTransaccionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia.viajes.transaccional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreTransaccionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
